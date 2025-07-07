78c9f8fdaf6cf8285d6111d8468238e3292b4122
vasim143 <vasim.tweet.349@gmail.com>
2025-07-07 15:05:03 +0530

    done to commit

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 provider  "aws" {
     region = "us-east-1"
 }
@@ -11,3 +12,16 @@ resource  "aws_instance" "example"{
         Name = var.tag_name
     }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "terraform_ec2" {
+  ami           = "ami-05ffe3c48a9991133"
+  instance_type = "t2.micro"
+  key_name      = "WAR2"
+
+  tags = {
+    Name = "TERRAFORM"
+
