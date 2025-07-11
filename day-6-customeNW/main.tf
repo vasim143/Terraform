084973742fe42994c8230c6c1070aac63f972eef
vasim143 <vasim.tweet.349@gmail.com>
2025-07-11 20:28:59 +0530

    Updated VPC setup and provider configuration

--- day-6-customeNW/main.tf
+++ day-6-customeNW/main.tf
@@ -1,119 +1,115 @@
 provider "aws" {
-    region = "us-east-1" 
+  region = "us-east-1"
 }
 
-resource "aws_vpc" "name" {
-  cidr_block = "10.0.2.0/16"
+resource "aws_vpc" "main" {
+  cidr_block = "10.1.0.0/16"
   tags = {
-    Name = "myvpc"
+    Name = "myvpc12"
   }
 }
 
-resource "aws_subnet" "name" {
-vpc_id = aws_vpc.name.id
-cidr_block = "10.0.2.0/24"
-availability_zone = "us-east-1a"
-tags = {
-    Name = "mysubnet"
-}
-}
-
-resource "aws_internet_gateway" "name" {
-    vpc_id = aws_vpc.name.id
-    tags = {
-        Name = "myIg"
-    } 
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.1.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "public-subnet1"
+  }
 }
 
-resource "aws_route_table" "name" {
- vpc_id =  aws_vpc.name.id
- route {
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "private-subnet1"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "myIG1"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.name.id
- }
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-rt1"
+  }
 }
 
-resource "aws_route_table_association" "name" {
-    subnet_id = aws_subnet.name.id
-    route_table_id = aws_route_table.name.id
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_security_group" "name" "allow_tls" {
-    name = "allow_tls"
-    vpc_id = aws_vpc.name.id
-    tags = {
-        Name = "mySG"
-    }
-}
+resource "aws_security_group" "allow_tls" {
+  name   = "allow_tls"
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "mySG1"
+  }
 
-ingress {
-    description = "TLS from VPC"
-    from_port = 80
-    to_port = 80
-    protocol = "TCP"
-    cidr_block = ["0.0.0.0/0"]
-}
   ingress {
-    description ="TLS from vpc"
-    from_port = 22
-    to_port = 22
-    protocol = "TCP"
-     cidr_block = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
-}
-
-egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1" #all protocols 
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-resource "aws_instance" "name" {
-    ami = "ami-05ffe3c48a9991133"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.name.id
-    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_subnet" "name" {
-vpc_id = aws_vpc.name.id
-cidr_block = "10.0.3.0/24"
-availability_zone = "us-east-1b"
-tags = {
-    Name = "myprivatesubnet"
-}
+resource "aws_eip" "nat" {
+   
 }
 
-resource "aws_nat_gateway" "name" {
-    allocation_id = aws_eip.name.id
-    subnet_id =  aws_subnet.name.id
-    tags = {
-        Name = "myNAT"
-    }
-     depends_on = [aws_internet_gateway.name]
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "myNAT1"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
 }
 
-resource "aws_nat_gateway" "name" {
-  allocation_id = aws_eip.mane.subnet_id
-  subnet_id = aws_subnet.name.id
-  secondary_allocation_ids = [aws_eip.secondary.id]
-  secondary_private_ip_address = ["10.0.3.0/24"] 
-}
-
-resource "aws_nat_gateway" "name" {
-    connectivity_type = "private"
-    subnet_id = aws_subnet.name.id
-}
-resource "aws_nat_gateway" "name" {
-  connectivity_type = "private"
-  subnet_id = aws_subnet.name.id
-  secondary_private_ip_address_count = 7
+resource "aws_instance" "example" {
+  ami                    = "ami-05ffe3c48a9991133"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  tags = {
+    Name = "my-instance"
+  }
 }
